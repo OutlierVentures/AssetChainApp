2f028bb005b12da5a45e0082fb0ab7adc8e3fd3b
AronVanAmmers <aron@certinio.com>
2015-05-15 18:39:52 +0200

    Verifications workflow (in progress)

--- contracts/asset-vault.sol
+++ contracts/asset-vault.sol
@@ -1,4 +1,10 @@
 ﻿contract AssetVault {
+    // TODO: update to recent Solidity developments
+    // - replace string32 by bytes32
+    // - replace mappings with counter by arrays as much as possible
+    // - (if possible) replace clumsy "find this or that" logic by functions, for
+    //  example getAsset(assetID).
+
     // Data structure to hold information about a single asset.
     struct Asset {
         // Unique AssetChain ID as used in cryptoledgers where it has been registered.
@@ -6,12 +12,34 @@
         // Descriptive name of the asset.
         // Could be removed once this is purely used as a secure data store.
         string32 name;
+
+        // List of verifications confirmed for this asset.
+        mapping (uint => Verification) verifications;
+        // Counter for access to the mapping.
+        uint verificationCount;
     }
 
     // Collection of assets and their count, to be used for one individual owner.
     struct AssetCollection {
         mapping (uint => Asset) assets;
         uint assetCount;
+    }
+
+    struct Verification {
+        // The user who verified the asset.
+        address verifier;
+        
+        // 1 = ownership
+        // 2 = quality
+        // ...
+        uint type;
+
+        // Whether the verification has been confirmed. Set to true after processing the verification.
+        bool isConfirmed;
+        
+        // Further data could include date of verification, state of the asset as it was at that point,
+        // etc. However these can all be retracted from the blockchain, assuming we can read the 
+        // contract storage history.
     }
 
     // The collection that stores all assets by owner address.
@@ -42,11 +70,11 @@
     }
 
     // Register an asset.
-    function createAsset(string32 id, string32 name) {
+    function createAsset(string32 id, string32 name) returns (bool dummyForLayout) {
         // Check: has the asset already been registered?
         if(ownerByAssetID[id] != 0x0)
-            // Asset with this ID has already been registered. No action.
-            return;
+        // Asset with this ID has already been registered. No action.
+        return;
 
         // Good to go. Register the asset.
         AssetCollection ac = assetsByOwner[tx.origin];
@@ -75,11 +103,17 @@
 
         return ac.assets[assetIndex].name;
     }
+
+    // Plumbing function to get an asset index within its collection.
+    // Currently not possible because return type must be a primitive type.
+    //function getAssetIndex(string32 assetID) returns (uint assetIndex){
+    //}
+
     // END plumbing functions to access properties of mappings that contain structs that 
     // contain mappings.
 	
     // Transfer ownership of an asset to a new owner. To be called by the prospective new owner.
-    function requestTransfer(string32 assetID) {
+    function requestTransfer(string32 assetID) returns (bool dummyForLayout) {
         // Check: is the asset ID valid?
         if(ownerByAssetID[assetID] == 0x0) {
             return;
@@ -102,7 +136,7 @@
     }
 
     // Confirm ownership transfer of an asset to a new owner. To be called by the current owner.
-    function processTransfer(string32 assetID, address newOwner, bool confirm) {
+    function processTransfer(string32 assetID, address newOwner, bool confirm) returns (bool dummyForLayout) {
         // Check: is the asset ID valid?
         if(ownerByAssetID[assetID] == 0x0) {
             return;
@@ -126,7 +160,7 @@
                 AssetCollection acOwner = assetsByOwner[tx.origin];
                 uint aIndex = 0;
                 while(aIndex <= acOwner.assetCount) {
-                    Asset a = acOwner.assets[aIndex];
+                    Asset a = acOwner.assets[aIndex++];
                     if(a.id == tr.assetID){
                         // If the owner confirms the request, transfer it. Otherwise just delete the request.
                         if(confirm) {
@@ -157,7 +191,6 @@
                         // Transfer completed, done.
                         return;
                     }
-                    aIndex++;
                 }
             }
 
@@ -170,9 +203,66 @@
     }
 
     // Remove expired transfer requests
-    function cleanTransferRequests(){
+    function cleanTransferRequests() returns (bool dummyForLayout) {
         // TODO: add a block number to TransferRequest so we can see when it expires.
         // TODO: implement
     }
+
+
+    // VERIFICATIONS
+
+    // Request verification of an asset.
+    function requestVerification(string32 assetID, address verifier, uint type) returns (bool dummyForLayout) {
+        // Check: is the asset ID valid?
+        if(ownerByAssetID[assetID] == 0x0) {
+            return;
+        }
+
+        // Check: is the verifier the current owner? That's not allowed.
+        if(tx.origin == verifier) {
+            return;
+        }
+
+        // Check: is the requester the current owner?
+        if(ownerByAssetID[assetID] != tx.origin) {
+            return;
+        }
+
+
+        // Get the asset
+        AssetCollection acOwner = assetsByOwner[ownerByAssetID[assetID]];
+            
+        uint aIndex = 0;
+        while(aIndex <= acOwner.assetCount) {
+            Asset a = acOwner.assets[aIndex++];
+            if(a.id == assetID){
+                // Add it to the asset verifications
+                Verification v = a.verifications[a.verificationCount++];
+                v.verifier = verifier;
+                v.type = type;
+            }
+        }
+    }
+
+    // Get info about a Verification of an asset.
+    function getVerification(string32 assetID, uint verificationIndex) returns (address verifier, uint type, bool isConfirmed) {
+        verifier = 0x0;
+        type = 1;
+        isConfirmed = false;
+    }
+
+    function processVerification(string32 assetID, address verifier, uint type, bool confirm) returns (bool dummyForLayout) {
+        // Check: is the asset ID valid?
+        if(ownerByAssetID[assetID] == 0x0) {
+            return;
+        }
+
+        // Find the verification
+
+        // Confirm? Set isConfirm to true.
+        // Not confirm? Set the verification to null, i.e. delete it.
+
+    }
+
 	
 }
